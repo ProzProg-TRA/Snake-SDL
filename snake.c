4b70b0cb7cdd79127771ade9c4df8ec7d4d65368
DaMightyZombie <42248344+DaMightyZombie@users.noreply.github.com>
2020-01-16 16:52:51 +0100

    begin creating input handling

--- snake.c
+++ snake.c
@@ -3,14 +3,24 @@
 
 #define GRID_RES_X 16
 #define GRID_RES_Y 9    
-#define GRID_CELL_SIZE 100
+#define GRID_CELL_SIZE 50
 
 SDL_Window* window;
 SDL_Renderer* renderer;
-
 SDL_Texture* bitmapTex;
 SDL_Surface* bitmapSurface;
+SDL_Event event;
 
+enum directions{
+    up      = 0,
+    down    = 1,
+    left    = 2,
+    right   = 3
+};
+
+enum directions snake_direction;
+int should_close;
+int pause;
 
 void draw()
 {
@@ -35,7 +45,33 @@ void draw()
 
 void step()
 {
-    
+    SDL_PollEvent(&event);
+    switch (event.type)
+    {
+    case SDL_QUIT:
+        should_close = 1;
+        break;
+    case SDL_KEYDOWN:
+        switch (event.key.keysym.scancode)
+        {
+        case SDL_SCANCODE_UP:
+            snake_direction = up;
+            break;
+        case SDL_SCANCODE_DOWN:
+            snake_direction = down;
+            break;
+        case SDL_SCANCODE_LEFT:
+            snake_direction = left;
+            break;
+        case SDL_SCANCODE_RIGHT:
+            snake_direction = right;
+            break;
+        default:
+            break;
+        }
+    default:
+        break;
+    }
 }
 
 int main()
@@ -56,7 +92,8 @@ int main()
     bitmapTex = SDL_CreateTextureFromSurface(renderer, bitmapSurface);
     SDL_FreeSurface(bitmapSurface);
 
-    int should_close = 0;
+    pause = 0;
+    should_close = 0;
 
     //main loop
     while (should_close == 0)
