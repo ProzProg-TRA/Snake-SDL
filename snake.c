79464da21c351df8e978d9de5964ce8d3005c2c9
DaMightyZombie <42248344+DaMightyZombie@users.noreply.github.com>
2020-01-16 15:52:21 +0100

    create res/ , res/red.bmp, started development of grid in snake.c

--- snake.c
+++ snake.c
@@ -5,9 +5,32 @@
 #define GRID_RES_Y 9    
 #define GRID_CELL_SIZE 100
 
+SDL_Window* window;
+SDL_Renderer* renderer;
+
+SDL_Texture* bitmapTex;
+SDL_Surface* bitmapSurface;
+
+
 void draw()
 {
+    SDL_RenderClear(renderer);
+    SDL_Rect rect;
 
+    for (int y = 0; y < GRID_RES_Y; y++)
+    {
+        for (int x = 0; x < GRID_RES_X; x++)
+        {
+            if ((x+y)%2)
+            {
+                rect.x = x * GRID_CELL_SIZE;
+                rect.y = y * GRID_CELL_SIZE;
+                rect.w = rect.h = GRID_CELL_SIZE;
+                SDL_RenderCopy(renderer, bitmapTex, NULL, &rect);
+            }
+        }
+    }
+    SDL_RenderPresent(renderer);
 }
 
 void step()
@@ -21,16 +44,24 @@ int main()
 		printf("error initializing SDL: %s\n", SDL_GetError()); 
 	}
 
-	SDL_Window* win = SDL_CreateWindow("Snake", // creates a window 
+	window = SDL_CreateWindow("Snake", // creates a window 
 									SDL_WINDOWPOS_CENTERED, 
 									SDL_WINDOWPOS_CENTERED, 
 									GRID_CELL_SIZE * GRID_RES_X, 
                                     GRID_CELL_SIZE * GRID_RES_Y, 0);
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    bitmapSurface = SDL_LoadBMP("res/red.bmp");
+    bitmapTex = SDL_CreateTextureFromSurface(renderer, bitmapSurface);
+    SDL_FreeSurface(bitmapSurface);
+
     int should_close = 0;
 
     //main loop
     while (should_close == 0)
     {
-        
+        step();
+        draw();
     }
 }
